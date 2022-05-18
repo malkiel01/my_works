6fced8b6f5b82efe6542e468329ead18be367d0c
malkiel01 <31856665+malkiel01@users.noreply.github.com>
2022-05-18 18:05:42 +0300

    test 02 - nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SimpleContainer from './componnents/SimpleContainer/SimpleContainer';
+import ResponsiveAppBar from './componnents/ResponsiveAppBar/ResponsiveAppBar';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar></ResponsiveAppBar>
      <SimpleContainer/>
     </div>
   );
